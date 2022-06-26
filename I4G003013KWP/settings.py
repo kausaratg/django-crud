f681ed41d0e89021fe2a6ec6f3fed43567112445
kassy <kausaratg@gmail.com>
2022-06-26 09:35:44 -0700

    final changes

--- I4G003013KWP/settings.py
+++ I4G003013KWP/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'I4G003013KWP.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
